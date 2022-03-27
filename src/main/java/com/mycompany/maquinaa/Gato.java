e55fd8b5bfd596f51c97031a9e2c07cbfd87e7bc
Marcelo Arrarte <marcelo.arrarte.ma@gmail.com>
2022-03-27 15:26:59 -0300

    Gato implements Mamifero

--- src/main/java/com/mycompany/maquinaa/Gato.java
+++ src/main/java/com/mycompany/maquinaa/Gato.java
@@ -1,12 +1,13 @@
 package com.mycompany.maquinaa;
 
-public class Gato {
+public class Gato implements Mamifero {
     private String nombre;
     
     public Gato(String nombre) {
         this.nombre = nombre;
     }
     
+    @Override
     public String getNombre() {
         return this.nombre;
     }
@@ -15,7 +16,23 @@ public class Gato {
         System.out.println("miau");
     }
     
+    @Override
     public boolean respondeA(String unNombre) {
         return false; // NUNCA RESPONDEN!
+    }
+    
+    @Override
+    public void caminar() {
+        System.out.println(this.nombre + " camina elegantemente");
+    }
+    
+    @Override
+    public void correr() {
+        System.out.println(this.nombre + " salió corriendo tras un pajarito");
+    }
+    
+    @Override
+    public void saltar() {
+        System.out.println(this.nombre + " saltó al techo del vecino");
     }
 }
